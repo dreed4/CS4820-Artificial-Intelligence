3a56ff6a916be69e24c4d84ea80eefb475f1c758
dreed4 <dreed4@uccs.edu>
2017-03-29 12:21:37 -0600

    added pseudo code

--- RePag Checkers/checkerboard.py
+++ RePag Checkers/checkerboard.py
@@ -8,8 +8,45 @@ class CheckerBoard(object):
     def __init__(self, arry):
         self.board = arry
     def getblack(self):
-        arry = self.arry
+        #move through whole 2d array and get 1's
+        arry = self.board
+        rows = len(self.board)
+        cols = len(self.board[0])
+        
+        for row in range(rows):
+            print(self.board[row])
     def getred(self):
-        arry= self.arry
+        #move through whole 2d array and get 3's
+        arry= self.board
     def printboard(self):
-        print("hello there")
+        #iterate through and print nicely
+        rows = len(self.board)
+        cols = len(self.board[0])
+        
+        for row in range(rows):
+            print(self.board[row])
+    def getmoves(self, piece):
+        print("getting moves")
+        # need to determine a standard for the "description" of a piece
+        # maybe a tuple like (coords, king=true/false) ?
+        
+        #if king, we know we can move forward or backward
+            #if space is on board
+                #if there space is occupied
+                    #our piece, can't move there
+                    
+                    #not our piece, see if we can jump (next diagonal must be empty)
+                
+                #if space not occupied, can move there
+            
+            
+        #if not a king, we can only move forward
+            #if space is on board
+                #if there space is occupied
+                    #our piece, can't move there
+                    
+                    #not our piece, see if we can jump (next diagonal must be empty)
+                
+                #if space not occupied, can move there
+    def movepiece(self, piece):
+        
