873a67cbd7d845e74eba1b8d12c52b373d214a21
dreed4 <dreed4@uccs.edu>
2017-05-07 17:59:36 -0600

    Heuritstic evaluation and refactoring

--- RePag Checkers/repagcheckers.py
+++ RePag Checkers/repagcheckers.py
@@ -27,75 +27,196 @@ def geninitcheckerboard ():
             [3,0,3,0,3,0,3,0],
             [0,3,0,3,0,3,0,3],
             [3,0,3,0,3,0,3,0]]
-    
-    arry = [[0,1,0,1,0,1,0,1],
-            [1,0,1,0,1,0,1,0],
-            [0,1,0,1,0,1,0,1],
-            [0,0,3,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [3,0,3,0,3,0,3,0],
-            [0,3,0,3,0,3,0,3],
-            [3,0,3,0,3,0,3,0]]
-    
-    arry = [[2,0,0,0,0,0,0,0],
-            [0,3,0,3,0,0,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,3,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,3,0,0],
-            [0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0]]
-    
+
     newboard = CheckerBoard(arry)
     
     return newboard
 
 def getminimaxscore(successor, prevscore, prevboard, currentcolor):
-    #lists of pieces of each color on board
-    #blacks = successor.getcolorpieces("black")
-    #reds = successor.getcolorpieces("red")
-    #previousblacks = prevboard.getcolorpieces("black")
-    #previousreds = prevboard.getcolorpieces("red")
-    
     blacks = successor.getcolorpieceswithkings("black")
     reds = successor.getcolorpieceswithkings("red")
-    previousblacks = prevboard.getcolorpieceswithkings("black")
-    previousreds = prevboard.getcolorpieceswithkings("red")
-    
-    #do color v color heuristic
-    #if currentcolor == "black":
-    #    colvcol = len(blacks) - len(reds)
-    #    currentpieces = blacks
-    #    oppcolorpieces = reds
-    #    oppcolor = "red"
-    #else:
-    #    colvcol = len(reds) - len(blacks)
-    #    currentpieces = reds
-    #    oppcolorpieces = blacks
-    #    oppcolor = "black"
-    #jump priority heuristic        
     
     if currentcolor == "black":
-        colvcol = (len(blacks[0]) + (len(blacks[1])*1.5)) - (len(reds[0]) + (len(reds[1])*1.5))
         currentpieces = blacks[0]
         currentpieces.extend(blacks[1])
         oppcolorpieces = reds[0]
         oppcolorpieces.extend(reds[1])
         oppcolor = "red"
+        
     else:
-        colvcol = (len(reds[0]) + (len(reds[1])*1.5)) - (len(blacks[0]) + (len(blacks[1])*1.5))
+        
         currentpieces = reds[0]
         currentpieces.extend(reds[1])
         oppcolorpieces = blacks[0]
         oppcolorpieces.extend(blacks[1])
         oppcolor = "black"
         
-    #if currentcolor == "black":
-    #    prevcolvcol = len(previousblacks) - len(previousreds)
-    #else:
-    #    prevcolvcol = len(previousreds) - len(previousblacks)
+    #flags
+    red_docolvcol = True
+    red_dodangerscore = True
+    red_dojumpscore = True
+    red_dopositionalscore = True
     
-    #jpriority = colvcol - prevcolvcol
+    black_docolvcol = False
+    black_dodangerscore = True
+    black_dojumpscore = True
+    black_dopositionalscore = True
+    
+    #init scores to 0
+    colvcolscore = 0
+    dangerscore = 0
+    jumpscore = 0
+    positionalscore = 0
+    
+    #get piece multiplier
+    pieceslost = 12 - len(currentpieces)
+    enemieslost = 12 - len(oppcolorpieces)
+    piecemultiplier = 1.2**pieceslost
+    enemypiecemultiplier = 1.2**enemieslost
+    
+    if currentcolor == "red":
+        if red_docolvcol == True:
+            colvcolscore = getcolvcolscore(blacks, reds, currentcolor, piecemultiplier, enemypiecemultiplier)
+        if red_dodangerscore == True:
+            dangerscore = getdangerscore(successor, currentcolor, currentpieces, oppcolorpieces)
+        if red_dojumpscore == True:
+            jumpscore = getjumpscore(successor, currentpieces, oppcolorpieces, oppcolor)
+        if red_dopositionalscore == True:
+            positionalscore = getpositionalscore(currentpieces, oppcolorpieces)
+        
+    elif currentcolor == "black":
+        if black_docolvcol == True:
+            colvcolscore = getcolvcolscore(blacks, reds, currentcolor, piecemultiplier, enemypiecemultiplier)
+        if black_dodangerscore == True:
+            dangerscore = getdangerscore(successor, currentcolor, currentpieces, oppcolorpieces)
+        if black_dojumpscore == True:
+            jumpscore = getjumpscore(successor, currentpieces, oppcolorpieces, oppcolor)
+        if black_dopositionalscore == True:
+            positionalscore = getpositionalscore(currentpieces, oppcolorpieces)
+    
+    #do weighting
+    colvcolweighted = colvcolscore * 5
+    dangerscoreweighted = -dangerscore
+    jumpscoreweighted = jumpscore
+    positionalscoreweighted = positionalscore
+    
+    totalscore = colvcolweighted + dangerscoreweighted + jumpscoreweighted + positionalscoreweighted
+    
+    return totalscore
+    
+def getcolvcolscore (blacks, reds, currentcolor, piecemultiplier, enemypiecemultiplier):
+    if currentcolor == "black":
+        colvcol = ((len(blacks[0]) + (len(blacks[1])*1.5))*piecemultiplier) - ((len(reds[0]) + (len(reds[1])*1.5))*enemypiecemultiplier)
+    else:
+        colvcol = ((len(reds[0]) + (len(reds[1])*1.5))*piecemultiplier) - ((len(blacks[0]) + (len(blacks[1])*1.5))*enemypiecemultiplier)
+        
+    colvcolpercentage = colvcol/18*100
+    
+    return colvcolpercentage
+
+def getdangerscore (successor, currentcolor, currentpieces, oppcolorpieces):
+    jboards = []
+    for p in oppcolorpieces:
+        jboards.extend(successor.getmaxdepthjumps(successor, p))
+    
+    dangerscores = []    
+    for jboard in jboards:
+        b = jboard[0]
+        amnt = len(b.getcolorpieces(currentcolor))
+        dangerscores.append((len(currentpieces)-len(oppcolorpieces)) - amnt)
+    
+    biggestdscore = 0
+    for dscore in dangerscores:
+        if dscore > biggestdscore:
+            biggestdscore = dscore
+    if (len(currentpieces)>0):        
+        totaldangerscore = biggestdscore/len(currentpieces)*100
+    else: 
+        totaldangerscore = 0
+        
+    return totaldangerscore
+
+def getjumpscore(successor, currentpieces, oppcolorpieces, oppcolor):
+    jboards = []
+    for p in currentpieces:
+        jboards.extend(successor.getmaxdepthjumps(successor, p))
+        
+    jumpscores = []
+    for jboard in jboards:
+        b = jboard[0]
+        amnt = len(b.getcolorpieces(oppcolor))
+        jumpscores.append(len(oppcolorpieces) - amnt)
+        
+    biggestjscore = 0
+    for jscore in jumpscores:
+        if jscore > biggestjscore:
+            biggestjscore = jscore
+    if (len(oppcolorpieces)):
+        totaljumpscore = biggestjscore/len(oppcolorpieces)*100
+    else:
+        totaljumpscore = 0
+    
+    return totaljumpscore
+
+def getpositionalscore(currentpieces, oppcolorpieces):
+    posscore = 0
+    if ((len(currentpieces) + len(oppcolorpieces)) > 8):
+        scores = [[1,1,1,1,1,1,1,1],
+                 [1,2,2,2,2,2,2,1],
+                 [1,2,3,3,3,3,2,1],
+                 [1,2,3,4,4,3,2,1],
+                 [1,2,3,4,4,3,2,1],
+                 [1,2,3,3,3,3,2,1],
+                 [1,2,2,2,2,2,2,1],
+                 [1,1,1,1,1,1,1,1]]
+    else:
+        scores = [[1,1,1,1,1,1,1,1],
+                 [1,3,3,3,3,3,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,3,3,3,3,3,1],
+                 [1,1,1,1,1,1,1,1]]
+    for p in currentpieces:
+        r = p[0]
+        c = p[1]
+        posscore = posscore + scores[r][c]
+        
+    posscore = posscore/32*100
+    
+    return posscore
+
+def getminimaxscore_old(successor, prevscore, prevboard, currentcolor):
+    #lists of pieces of each color on board
+    blacks = successor.getcolorpieceswithkings("black")
+    reds = successor.getcolorpieceswithkings("red")
+      
+    
+    if currentcolor == "black":
+        currentpieces = blacks[0]
+        currentpieces.extend(blacks[1])
+        oppcolorpieces = reds[0]
+        oppcolorpieces.extend(reds[1])
+        oppcolor = "red"
+        pieceslost = 12 - len(currentpieces)
+        enemieslost = 12 - len(oppcolorpieces)
+        piecemultiplier = 1.2**pieceslost
+        enemypiecemulitplier = 1.2**enemieslost
+        colvcol = ((len(blacks[0]) + (len(blacks[1])*1.5))*piecemultiplier) - ((len(reds[0]) + (len(reds[1])*1.5))*enemypiecemulitplier)
+    else:
+        
+        currentpieces = reds[0]
+        currentpieces.extend(reds[1])
+        oppcolorpieces = blacks[0]
+        oppcolorpieces.extend(blacks[1])
+        oppcolor = "black"
+        pieceslost = 12 - len(currentpieces)
+        enemieslost = 12 - len(oppcolorpieces)
+        piecemultiplier = 1.2**pieceslost
+        enemypiecemulitplier = 1.2**enemieslost
+        colvcol = ((len(reds[0]) + (len(reds[1])*1.5))*piecemultiplier) - ((len(blacks[0]) + (len(blacks[1])*1.5))*enemypiecemulitplier)
+   
     
     #board safety
     jboards = []
@@ -116,42 +237,70 @@ def getminimaxscore(successor, prevscore, prevboard, currentcolor):
         totaldangerscore = biggestdscore/len(currentpieces)*100
     else: 
         totaldangerscore = 0
-    #max for any is going to be 100
     
-
+    #jump scores
+    jboards = []
+    for p in currentpieces:
+        jboards.extend(successor.getmaxdepthjumps(successor, p))
+        
+    jumpscores = []
+    for jboard in jboards:
+        b = jboard[0]
+        amnt = len(b.getcolorpieces(oppcolor))
+        jumpscores.append(len(successor.getcolorpieces(oppcolor)) - amnt)
+        
+    biggestjscore = 0
+    for jscore in jumpscores:
+        if jscore > biggestjscore:
+            biggestjscore = jscore
+    if (len(oppcolorpieces)):
+        totaljumpscore = biggestjscore/len(oppcolorpieces)*100
+    else:
+        totaljumpscore = 0
 
     #position scoring
     #posmax : 32
     posscore = 0
-    scores = [[1,1,1,1,1,1,1,1],
-             [1,2,2,2,2,2,2,1],
-             [1,2,3,3,3,3,2,1],
-             [1,2,3,4,4,3,2,1],
-             [1,2,3,4,4,3,2,1],
-             [1,2,3,3,3,3,2,1],
-             [1,2,2,2,2,2,2,1],
-             [1,1,1,1,1,1,1,1]]
+    if ((len(currentpieces) + len(oppcolorpieces)) > 8):
+        scores = [[1,1,1,1,1,1,1,1],
+                 [1,2,2,2,2,2,2,1],
+                 [1,2,3,3,3,3,2,1],
+                 [1,2,3,4,4,3,2,1],
+                 [1,2,3,4,4,3,2,1],
+                 [1,2,3,3,3,3,2,1],
+                 [1,2,2,2,2,2,2,1],
+                 [1,1,1,1,1,1,1,1]]
+    else:
+        scores = [[1,1,1,1,1,1,1,1],
+                 [1,3,3,3,3,3,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,5,5,5,5,3,1],
+                 [1,3,3,3,3,3,3,1],
+                 [1,1,1,1,1,1,1,1]]
     for p in currentpieces:
         r = p[0]
         c = p[1]
         posscore = posscore + scores[r][c]
         
     posscore = posscore/32*100
-    #for colvcol, max would be 12
+    
+    #for colvcol, max would be 18
     colvcolpercentage = colvcol/18*100
     
-    #jprioritypercentage = jpriority/9*100
-    
     colvcolwithmult = colvcolpercentage*5
-    #jprioritywithmult = jprioritypercentage*0.9
     
     dangerscorewithmult = totaldangerscore
+    
+    jumpscorewithmult = totaljumpscore
     if len(currentpieces) > 0:
         posscorewithmult = posscore*(1/len(currentpieces))
     else:
         posscorewithmult = posscore*0
+        
     #max finalscore = 10
-    finalscore = colvcolwithmult + (-dangerscorewithmult) + posscorewithmult#+ jprioritywithmult
+    finalscore = colvcolwithmult + (-dangerscorewithmult) + posscorewithmult + jumpscorewithmult
     if len(currentpieces) <= 0:
         finalscore = -math.inf
     return finalscore
@@ -165,25 +314,6 @@ def getminimaxscore_single_old(successor, prevscore, prevboard, currentcolor):
         currentscore = len(reds) - len(blacks)
         
     return currentscore
-    
-def getminimaxscore_old(successor, prevscore, prevboard, currentcolor):
-    currentblacks = successor.getcolorpieces("black")
-    currentreds = successor.getcolorpieces("red")
-    previousblacks = prevboard.getcolorpieces("black")
-    previousreds = prevboard.getcolorpieces("red")
-    
-    if currentcolor == "black":
-        currentscore = len(currentblacks) - len(currentreds)
-        previousscore = len(previousblacks) - len(previousreds)
-    else:
-        currentscore = len(currentreds) - len(currentblacks)
-        previousscore = len(previousreds) - len(previousblacks)
-    #current score - previous score
-    
-    
-    finalscore = currentscore - previousscore
-    
-    return finalscore
 
 def minplay (successor, depth, maxdepth, maxicolor, maxtime, starttime,prevscore, alpha, beta, prevboard):
     #placeholder var
@@ -328,6 +458,7 @@ def id(board, maxdepth, maxicolor, aitype, maxtime):
         
     
     return bestboard
+
 def graphsearch (rootnode, blacksearchtype, blackaitype, redsearchtype, redaitype, maxdepth,maxtimeblack,maxtimered,q):
     #first move player
     currentplayer = "black"
@@ -369,18 +500,7 @@ def graphsearch (rootnode, blacksearchtype, blackaitype, redsearchtype, redaityp
             
     return (board, winner, numturns)
     
-def test ():
-    newboard = geninitcheckerboard()
-    print("")
-    newboard.printboard()
-    successors = newboard.getsuccessors("black")
-    print (successors)
-    for s in successors:
-        print("")
-        s.printboard()
-    
-  
-    
+
     
 def checkersgame(q, rootarry):
     #init gui stuff first
@@ -401,8 +521,8 @@ def checkersgame(q, rootarry):
     rootnode = CheckerBoard(rootarry)
     rootnode.printboard()
     print("")
-    maxtimeperturnblack = 10
-    maxtimeperturnred = 10
+    maxtimeperturnblack = 6
+    maxtimeperturnred = 6
     
     blacksearchtype = id
     blackaitype = minimax
@@ -423,6 +543,8 @@ def checkersgame(q, rootarry):
     finalboard.printboard()
     print("Number of turns: ", numturns)
     print("Total time taken (seconds): ", totaltime)
+    
+#old calls
 #print("calling repagcheckers file outside")
 #checkersgame(q=None)
     
